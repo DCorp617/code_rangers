5dea10df935acc0b2b68dfbf6a01dd9270fa661b
melissadi <m.w.straus@gmail.com>
2019-07-15 15:45:30 -0400

    Updated User table, signup form, and user_sign_up_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
+require_relative './constants'
+
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :home_state, inclusion: { in: STATES }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
