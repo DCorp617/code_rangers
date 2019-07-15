45941a0014cdc6e7f2592df6efb2043f79a85f62
melissadi <m.w.straus@gmail.com>
2019-07-15 17:08:51 -0400

    Created tests for User model and user sign in and updated FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a User if all required fields on signup page are completed" do
+    user = FactoryBot.create(:user)
+
+    expect(user.save).to be true
+  end
+
+  it "raise an error if all required fields on signup page are not completed" do
+    user = User.create(first_name: 'Scott', email: "scottjohn@aol.com")
+
+    expect(user.save).to be false
+  end
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -5,6 +5,8 @@ FactoryBot.define do
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    first_name { 'joe' }
+    last_name { 'brown' }
+    home_state { 'PA'}
   end
-
 end
