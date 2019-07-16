fe28cc84dd65cfce7e7beccbb9d9ef65bff2d14c
Paul RocheteauE <rocheteau.p@gmail.com>
2019-07-16 10:24:02 -0400

    Implemented Shoulda Gem for City test

--- spec/features/02_visitor_sees_city_information_spec.rb
+++ spec/features/02_visitor_sees_city_information_spec.rb
@@ -5,8 +5,7 @@ feature "visitor sees city information" do
     state1 = State.create!(name:"Maine", description:"Lovely place to be in tune with nature")
 
     city1 = City.create!(name:"Orono", description:"Home of Univerity of Maine, biggest super computer on a campus", state: state1)
-
-    visit "/cities/1"
+    visit "/cities/#{city1.id}"
 
     expect(page).to have_content "Orono"
     expect(page).to have_content "Home of Univerity of Maine, biggest super computer on a campus"
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'shoulda-matchers'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -61,4 +62,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  Shoulda::Matchers.configure do |config|
+      config.integrate do |with|
+        with.test_framework :rspec
+        with.library :rails
+        with.library :active_record
+        with.library :active_model
+      end
+  end
 end
