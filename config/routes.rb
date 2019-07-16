92a2d1b393f3b9bc67dbcfaa4ab786454e42862b
Paul RocheteauE <rocheteau.p@gmail.com>
2019-07-16 14:57:31 -0400

    Created route and index page for parks, created feature test to view parks index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'states#index'
   devise_for :users
 
-  resources :states, only: [:index]
+  resources :states, only: [:index, :show]
+  resources :cities, only: [:index, :show]
+  resources :parks, only: [:index, :show]
 
-  resources :cities, only: [:show]
 end
