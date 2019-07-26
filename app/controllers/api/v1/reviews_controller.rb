c7f6db2b2a32db9459eec428034b336a1f3f8fae
Paul RocheteauE <rocheteau.p@gmail.com>
2019-07-26 12:08:30 -0400

    Progress on adding Flash Error Messages

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -9,12 +9,32 @@ class Api::V1::ReviewsController < ApplicationController
   def create
     parkId = params[:park_id]
     @park = Park.find(parkId)
+    parsed_review = JSON.parse(request.body.read)
+    @review = Review.new()
+    @review.user = current_user
+    @review.description = parsed_review["description"]
+    @review.rating = parsed_review["rating"]
+    @review.reviewable = Park.find(parkId)
+    binding.pry
 
-    review = JSON.parse(request.body.read)
-
-    Review.create!(user: current_user, description: review["description"], rating: review["rating"], reviewable: Park.find(parkId))
-
-    render json: Park.find(parkId)
+    if @review.save
+     flash[:notice] = "Review added successfully"
+     render json: Park.find(parkId)
+    else
+     flash.now[:error] = @review.errors.full_messages.join(", ")
+     binding.pry
+     render json: Park.find(parkId)
+    end
   end
-
 end
+
+# def create
+#   parkId = params[:park_id]
+#   @park = Park.find(parkId)
+#
+#   review = JSON.parse(request.body.read)
+#
+#   Review.create!(user: current_user, description: review["description"], rating: review["rating"], reviewable: Park.find(parkId))
+#
+#   render json: Park.find(parkId)
+# end
